eeeaeba9c593a6b411a3a088681d67c4d5c4c649
Christopher Williams <chrsw49@gmail.com>
2015-11-03 16:19:54 -0800

    created UIImage extension for reading pixel color

--- iOSImageDetectionExample/iOSImageDetectionExample/ViewController.swift
+++ iOSImageDetectionExample/iOSImageDetectionExample/ViewController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+// http://stackoverflow.com/questions/25146557/how-do-i-get-the-color-of-a-pixel-in-a-uiimage-with-swift
+extension UIImage {
+    func getPixelColor(pos: CGPoint) -> UIColor {
+        // Read unscaled image's pixel data
+        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.CGImage))
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        // Which pixel index is the parameter wanting us to look at?
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -22,4 +41,3 @@ class ViewController: UIViewController {
 
 
 }
-
